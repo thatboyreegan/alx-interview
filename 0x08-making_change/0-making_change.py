7f95872364a6d22c298884e335065d8d2e2bbd97
Reagan Ronald Odhiambo <reaganronald491@gmail.com>
2024-03-01 05:21:15 +0300

    made changes to the styling

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,21 +1,22 @@
 #!/usr/bin/python3
 """Change comes from within """
 
+
 def makeChange(coins, total):
     """
-    determines the fewest number of 
-    coins needed to meet a given amount total 
+    determines the fewest number of
+    coins needed to meet a given amount total
     given a pile of coins of different values
     """
     if total <= 0:
         return 0
-    
+
     rem = total
     coinCount = 0
     coinIndex = 0
     sortedCoins = sorted(coins, reverse=True)
     i = len(coins)
-    
+
     while rem > 0:
         if coinIndex >= i:
             return -1
